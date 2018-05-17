7368b5dc88331eb9889d9e50ef7e07a3f01185d9
kuuzu <39206009+kuuzu@users.noreply.github.com>
2018-05-17 16:25:56 +1000

    Add files via upload

---
+++ includes/modules/content/product_info/templates/reviews.php
@@ -0,0 +1,9 @@
+<?php
+use KUU\ZU\KUUZU;
+
+?>
+<div class="col-sm-<?php echo $content_width; ?> reviews">
+  <h4 class="page-header"><?php echo KUUZU::getDef('reviews_text_title'); ?></h4>
+  <?php echo $review_data; ?>
+</div>
+
