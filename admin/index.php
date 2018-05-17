1b32334c516670dc9bd34d58209cedb12a9c4c8c
kuuzu <39206009+kuuzu@users.noreply.github.com>
2018-05-17 12:31:10 +1000

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,90 @@
+<?php
+/**
+  * osCommerce Online Merchant
+  *
+  * @copyright (c) 2016 osCommerce; https://www.oscommerce.com
+  * @license MIT; https://www.oscommerce.com/license/mit.txt
+  */
+
+  use KUU\ZU\Apps;
+  use KUU\ZU\HTML;
+  use KUU\ZU\KUUZU;
+
+  require('includes/application_top.php');
+
+  if (KUUZU::hasSitePage()) {
+    if (KUUZU::isRPC() === false) {
+        $page_file = KUUZU::getSitePageFile();
+
+        if (empty($page_file) || !is_file($page_file)) {
+          $page_file = KUUZU::getConfig('dir_root', 'Shop') . 'includes/error_documents/404.php';
+        }
+
+        if (KUUZU::useSiteTemplateWithPageFile()) {
+          include($kuuTemplate->getFile('template_top.php'));
+        }
+
+        include($page_file);
+
+        if (KUUZU::useSiteTemplateWithPageFile()) {
+          include($kuuTemplate->getFile('template_bottom.php'));
+        }
+    }
+
+    goto main_sub3;
+  }
+
+  require($kuuTemplate->getFile('template_top.php'));
+?>
+
+<h2><i class="fa fa-home"></i> <a href="<?= KUUZU::link(FILENAME_DEFAULT); ?>"><?= STORE_NAME; ?></a></h2>
+
+<?php
+  if (defined('MODULE_ADMIN_DASHBOARD_INSTALLED') && tep_not_null(MODULE_ADMIN_DASHBOARD_INSTALLED)) {
+    $adm_array = explode(';', MODULE_ADMIN_DASHBOARD_INSTALLED);
+
+    $col = 0;
+
+    foreach ($adm_array as $adm) {
+      if (strpos($adm, '\\') !== false) {
+        $class = Apps::getModuleClass($adm, 'AdminDashboard');
+      } else {
+        $class = substr($adm, 0, strrpos($adm, '.'));
+
+        if ( !class_exists($class) ) {
+          $KUUZU_Language->loadDefinitions('modules/dashboard/' . pathinfo($adm, PATHINFO_FILENAME));
+
+          include('includes/modules/dashboard/' . $class . '.php');
+        }
+      }
+
+      $ad = new $class();
+
+      if ($ad->isEnabled()) {
+        $col += 1;
+
+        if ($col === 1) {
+          echo '<div class="row">';
+        }
+
+        echo '<div class="col-md-6">' . $ad->getOutput() . '</div>';
+
+        if ($col === 2) {
+          $col = 0;
+
+          echo '</div>';
+        }
+      }
+    }
+
+    if ($col === 1) {
+      echo '</div>';
+    }
+  }
+
+  require($kuuTemplate->getFile('template_bottom.php'));
+
+  main_sub3: // Sites and Apps skip to here
+
+  require('includes/application_bottom.php');
+?>
---
+++ admin/products_attributes.php
@@ -0,0 +1,18 @@
+<?php
+/**
+  * osCommerce Online Merchant
+  *
+  * @copyright (c) 2016 osCommerce; https://www.oscommerce.com
+  * @license MIT; https://www.oscommerce.com/license/mit.txt
+  */
+
+  require('includes/application_top.php');
+  require($kuuTemplate->getFile('template_top.php'));
+?>
+
+<p>This page will return.</p>
+
+<?php
+  require($kuuTemplate->getFile('template_bottom.php'));
+  require('includes/application_bottom.php');
+?>
